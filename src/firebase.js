8e99b6f85bd314a7f36e404bdc432059f475df78
Софія Дацко <soffia242004@gmail.com>
2026-05-13 21:36:01 +0300

    fix: force account selection in firebase provider

--- src/firebase.js
+++ src/firebase.js
@@ -17,5 +17,8 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({ prompt: 'select_account' }); 
+
 export const db = getFirestore(app);
 export const storage = getStorage(app);
